f88e9783fa29db9b7dcb320ad7e4fd33dd15f121
eugenkyky <eugenkyky@gmail.com>
2016-09-05 12:36:55 +0500

    Vagrant added

---
+++ bootstrap.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#PHP install
+sudo apt-get update &&
+sudo apt-get install python-software-properties -y &&
+sudo LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php -y &&
+sudo apt-get update &&
+sudo apt-get install php7.0 php7.0-mysql curl git libpcre3 zip unzip php7.0-xml -y &&
+(curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer) &&
+#MYSQL install
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password qwer1234'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password qwer1234'
+sudo apt-get install mysql-server -y &&
+sudo apt-get install mysql-client -y &&
+
+'''
+    Требования к скрипту
+
+    1. Установка сифони
+    2. Скачивание приложения
+    3. Миграция
+    4. Запуск
+'''
+
+git clone https://github.com/eugenkyky/test_repo.git test_assignment &&
+#composer get additional packages
+cd test_assignment &&
+sudo chown vagrant tests &&
+sudo composer install &&
+#install db scheme
+mysql -uroot -pqwer1234 -e "CREATE DATABASE 4xii;" &&
+#create schema
+php bin/console doctrine:schema:update --force
+php bin/console server:run
+echo "Deploy task succeded" &&
+exit 0
+
+
+
+
+
+
+
+
